d0027dcc914a1ac5b5c164b620378f2769a8ec55
Anton Trollbäck <anton@trollback.se>
2015-07-27 17:55:15 +0200

    add footer

--- src/templates/partials/footer.php
+++ src/templates/partials/footer.php
@@ -1,4 +1,11 @@
 
+  <footer class="Footer">
+    <div class="Site-container">
+      <p class="Footer-column"><?php bloginfo( 'name' ); echo ' &#8211; '; bloginfo( 'description' ); ?></p>
+      <p class="Footer-column"><a class="u-link" href="mailto:info@settings.se">info@settings.se</a></p>
+    </div>
+  </footer>
+
   <script src="<?php asset_url( 'scripts/settings.js' ); ?>"></script>
 
   <?php wp_footer(); ?>
