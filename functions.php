53fbe6bcab8053dd9e1f80b810b580c075e2f941
Anton Trollbäck <anton@trollback.se>
2015-07-30 02:18:06 +0200

    minor design fixes

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function logga ( $data ) {
   echo '</pre>';
 }
 
-$revision = 0.0.1;
+$revision = '0.6';
 $fb_token = file_get_contents(get_template_directory() . '/fb-token.txt');
 
 require get_template_directory() . '/inc/setup.php';
--- src/templates/post.php
+++ src/templates/post.php
@@ -3,11 +3,13 @@
 <?php while ( have_posts() ) : the_post(); ?>
   <main role="main">
     <article class="Site-container Site-container--withRoomForNav">
-      <figure class="Section Section--full">
-        <a class="u-inlineBlock" href="<?php raw_image_url(); ?>">
-          <?php image( 'large', 'Section-photo', true); ?>
-        </a>
-      </figure>
+      <?php if ( has_post_thumbnail() ): ?>
+        <figure class="Section Section--full">
+          <a class="u-inlineBlock" href="<?php raw_image_url(); ?>">
+            <?php image( 'large', 'Section-photo', true); ?>
+          </a>
+        </figure>
+      <?php endif; ?>
 
       <?php partial( 'article' ); ?>
 
